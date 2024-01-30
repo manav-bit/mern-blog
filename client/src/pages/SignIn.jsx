71eb88935c90cf9845d5b10c738d0e9222e68b5a
MANAV AHUJA <ahujamanav43@gmail.com>
2024-01-31 00:17:47 +0530

    add redux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -2,10 +2,15 @@
 import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react"
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom"
+import {  useDispatch, useSelector } from "react-redux";
+import { signInFailure,signInStart,signInSuccess } from "../redux/user/userSlice";
+
 export default function SignIn() {
   const [formData,setFormData]=useState({});
-  const [errorMessage,setErrorMessage]=useState((null));
-  const [loading,setLoading]=useState((false));
+  // const [errorMessage,setErrorMessage]=useState((null));
+  // const [loading,setLoading]=useState((false));
+  const {loading,error:errorMessage}=useSelector(state=>state.user);
+  const dispatch=useDispatch();
   const navigate=useNavigate();
 const handleChange=(e)=>{
 setFormData({...formData,[e.target.id]: e.target.value.trim()});
@@ -14,11 +19,13 @@ setFormData({...formData,[e.target.id]: e.target.value.trim()});
 const handleSubmit= async(e)=>{
   e.preventDefault();
   if(!formData.email||!formData.password){
-    return setErrorMessage('Please fill out all fields')
+    // return setErrorMessage('Please fill out all fields')
+    return dispatch(signInFailure('Please fill all the fields'));
   }
   try{
-    setLoading(true);
-    setErrorMessage(null);
+    // setLoading(true);
+    // setErrorMessage(null);
+    dispatch(signInStart());
 const res=await fetch('/api/auth/signin',{
   method:'POST',
   headers:{'Content-Type':'application/json'},
@@ -26,16 +33,20 @@ const res=await fetch('/api/auth/signin',{
 });
 const data=await res.json();
 if(data.success===false){
-  return setErrorMessage(data.message);
+  // return setErrorMessage(data.message);
+  dispatch(signInFailure(data.message));
 }
-setLoading(false);
+// setLoading(false);
 if(res.ok){
+  dispatch(signInSuccess(data));
+
   navigate('/');
 }
   }
   catch(error){
-    setErrorMessage(error.message);
-    setLoading(false);
+    dispatch(signInFailure(error.message));
+    // setErrorMessage(error.message);
+    // setLoading(false);
   }
 
 }
@@ -92,7 +103,7 @@ Blog
 
 <div className="flex gap-2 text-sm mt-5">
   <span>
-   Don't have an account?
+   Dont have an account?
   </span>
   <Link to='/sign-up' className="text-blue-500" >
     Sign Up
