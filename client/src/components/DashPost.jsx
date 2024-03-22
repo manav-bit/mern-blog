0f8311f69500f66b18f8322500673480d23a7662
MANAV AHUJA <ahujamanav43@gmail.com>
2024-03-22 15:20:35 +0530

    add show more functionality to the posts results of the dashboard

--- client/src/components/DashPost.jsx
+++ client/src/components/DashPost.jsx
@@ -4,6 +4,7 @@ import {Table} from 'flowbite-react';
 import {Link} from 'react-router-dom';
 export default function DashPost() {
   const {currentUser} = useSelector((state)=>state.user)
+  const [showMore,setShowMore] = useState(true);
   const [userPosts,setUserPosts]=useState([]);
   console.log(userPosts);
   useEffect(()=>{
@@ -13,6 +14,9 @@ export default function DashPost() {
         const data=await res.json()
         if(res.ok){
           setUserPosts(data.posts)
+          if(data.posts.length<9){
+            setShowMore(false);
+          }
         }
         console.log(data.posts);
       }catch(error){
@@ -23,6 +27,22 @@ export default function DashPost() {
       fetchPosts();
     }
   },[currentUser._id]);
+  const handleShowMore = async()=>{
+    const startIndex=userPosts.length;
+    try{
+ const res=await fetch(`/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`);
+ const data=await res.json();
+ if(res.ok){
+  setUserPosts((prev)=>[...prev,...data.posts]);
+  if(data.posts.length<9){
+    setShowMore(false);
+  }
+ }
+    }
+    catch(error){
+console.log(error.message);
+    }
+  }
   return (
     <div className="table-auto overflow-x-scroll md:mx-auto p-3 scrollbar
     scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700
@@ -86,6 +106,14 @@ className="w-20 h-20 object-cover bg-gray-500"
   </Table.Body>
 ))}
   </Table>
+  {
+    showMore &&(
+    <button onClick={handleShowMore} className="w-full text-teal-500 self-center text-sm py-7">
+      Show more
+      </button>
+      )
+
+  }
   </>
 ):(
   <p> You have no posts yet!</p>
